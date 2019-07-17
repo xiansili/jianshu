24821c3815e20e7104465356388b995b88d38dab
xiansili <865915497@qq.com>
2019-07-17 18:52:01 +0800

    init

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
-import { HeaderWrapper } from "./style";
+import logoPic from "../../static/logo.png";
+import {
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addtion,
+  Button,
+  Wrapper
+} from "./style";
 
 class Header extends Component {
   render() {
     return (
-    <HeaderWrapper>header</HeaderWrapper>
+      <HeaderWrapper>
+        <Wrapper>
+          <Logo>
+              <img src={logoPic} alt="logo"/>
+          </Logo>
+          <Nav>
+            <NavItem className="left active">首页</NavItem>
+            <NavItem className="left">下载APP</NavItem>
+            <NavItem className="right">登录</NavItem>
+            <NavItem className="right">Aa</NavItem>
+            <NavSearch />
+          </Nav>
+          <Addtion>
+            <Button>注册</Button>
+            <Button>写文章</Button>
+          </Addtion>
+        </Wrapper>
+      </HeaderWrapper>
     );
   }
 }
--- jianshu/src/common/header/style.js
+++ jianshu/src/common/header/style.js
@@ -1,5 +1,95 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-  color: red;
+  height: 56px;
+  position: fixed;
+  top:0;
+  left:0;
+  right:0;
+  background-color: #fff;
+  border-bottom: 1px solid #f0f0f0;
+  margin-bottom: 20px;
+`;
+
+export const Wrapper = styled.div`
+  width:1440px;
+  margin: 0 auto;
+  position:relative;
+  height:100%;
+`;
+
+export const Logo = styled.a.attrs({
+  href: "/"
+})`
+  height: 56px; 
+  width: 100px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  & img{
+      width:100%;
+      height:100%;
+  }
+`;
+
+export const Nav = styled.div`
+  height: 100%;
+  width: 960px;
+  margin: 0 auto;
+`;
+
+export const NavItem = styled.div`
+  line-height: 56px;
+  padding: 0 15px;
+  font-size: 17px;
+  margin-right:10px;
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right;
+  }
+  &.active {
+    color: #ea6f5a;
+  }
+`;
+
+export const NavSearch = styled.input.attrs({
+  placeholder: "搜索"
+})`
+  height: 38px;
+  width: 160px;
+  border-radius: 19px;
+  outline: none;
+  border: none;
+  margin-top: 8px;
+  margin-left: 10px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  background: #eee;
+  font-size: 14px;
+  &:: {
+    color: #999;
+  }
+`;
+
+export const Addtion = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 56px;
+`;
+
+export const Button = styled.div`
+  float: left;
+  width: 80px;
+  height: 38px;
+  line-height: 24px;
+  margin: 9px 5px 0 15px;
+  border: 1px solid rgba(236, 97, 73, 0.7);
+  border-radius: 20px;
+  font-size: 15px;
+  color: #ea6f5a;
+  background-color: transparent;
 `;
